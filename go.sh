af6dbf9f37922f8f47dd90a3734cb0fb7e339be0
云乾 <yunqian@cncfstack.com>
2025-09-18 11:22:52 +0800

    Date=>2025年 9月18日 星期四 11时22分52秒 CST

--- go.sh
+++ go.sh
@@ -2,5 +2,6 @@
 DATE=$(date)
 echo "Date=>$DATE" > .date
 git add . --all && git commit -m "Date=>$DATE" && git push
-sleep 3
+sleep 10
+ps aux |grep '8888:localhost:9999'|awk '{print $2}'|xargs kill -9
 ssh -N -f -g -o ServerAliveInterval=60  -L 8888:localhost:9999  aliyun2
